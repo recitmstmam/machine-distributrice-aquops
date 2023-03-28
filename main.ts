12b1a04635b31d679d38b272c0d7d81efefc6bff
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2023-03-28 19:39:49 -0400

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,8 +2,25 @@ input.onButtonPressed(Button.A, function () {
     servos.P0.setAngle(59)
 })
 input.onButtonPressed(Button.B, function () {
-    servos.P0.setAngle(95)
+    servos.P0.setAngle(93)
 })
+servos.P0.setAngle(94)
 basic.forever(function () {
-	
+    serial.writeValue("d", sonar.ping(
+    DigitalPin.P2,
+    DigitalPin.P1,
+    PingUnit.Centimeters
+    ))
+    if (sonar.ping(
+    DigitalPin.P2,
+    DigitalPin.P1,
+    PingUnit.Centimeters
+    ) <= 5) {
+        servos.P0.setAngle(59)
+        basic.pause(500)
+        servos.P0.setAngle(93)
+        basic.pause(2000)
+    } else {
+        servos.P0.setAngle(93)
+    }
 })
