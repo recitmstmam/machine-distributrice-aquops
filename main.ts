571c1290917b4c7745a36a4716d566d08963e978
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2023-03-28 17:59:41 -0400

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    servos.P0.setAngle(60)
+})
+input.onButtonPressed(Button.B, function () {
+    servos.P0.setAngle(92)
+})
+basic.forever(function () {
+	
+})
