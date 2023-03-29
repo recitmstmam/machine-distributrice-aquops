23ab934c633470866592e9777691805d3e72bfd0
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2023-03-29 13:38:36 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 input.onButtonPressed(Button.A, function () {
-    servos.P0.setAngle(59)
+    servos.P0.setAngle(60)
 })
 input.onButtonPressed(Button.B, function () {
-    servos.P0.setAngle(93)
+    servos.P0.setAngle(96)
 })
-servos.P0.setAngle(94)
+servos.P0.setAngle(60)
 basic.forever(function () {
     serial.writeValue("d", sonar.ping(
     DigitalPin.P2,
@@ -16,12 +16,12 @@ basic.forever(function () {
     DigitalPin.P1,
     PingUnit.Centimeters
     ) <= 6) {
-        servos.P0.setAngle(59)
+        servos.P0.setAngle(96)
         basic.pause(500)
-        servos.P0.setAngle(93)
+        servos.P0.setAngle(60)
         basic.pause(1000)
     } else {
-        servos.P0.setAngle(93)
+        servos.P0.setAngle(60)
     }
     basic.pause(1000)
 })
